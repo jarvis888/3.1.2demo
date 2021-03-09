b6c82a9c00a6a8d9ae80a4a77bb52213a0723821
John Hurdle <john.renken@lambdaschool.com>
2021-03-09 11:27:28 -0800

    first commit

--- reddit.py
+++ reddit.py
@@ -1,5 +1,4 @@
 # users, comments, posts
-import pandas
 
 
 class User:
@@ -9,17 +8,25 @@ class User:
         self.reputation = reputation
         self.mod_status = mod_status
         self.can_post = can_post
+        self.post_topic('my first topic', ['automated'], 'this topic is automatically created for every new user')
 
     def post_topic(self, title, tags, description):
-        topic = Topic(title, tags, description)
+        author = self.username
+        topic = Topic(title, tags, description, author)
+        return topic
+
+    @staticmethod
+    def post_topic_static(title, tags, description, author):
+        topic = Topic(title, tags, description, author)
         return topic
 
 
 class Topic:
-    def __init__(self, title, tags, description):
+    def __init__(self, title, tags, description, author):
         self.title = title
         self.tags = tags
         self.description = description
+        self.author = author
 
 
 class Comment:
@@ -36,3 +43,6 @@ sarah.post_topic(
     ["food", "healthy"],
     "smoothies are the best. love smoothies. can't get enough smoothies.",
 )
+
+
+User.post_topic_static('football is cool', ['sports'], 'tom brady\'s take on football', 'tombrady99')
